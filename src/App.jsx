713f9201a560fa483d55bab17698128c9b268c50
luisda13 <luisda.martinez.111@gmail.com>
2025-08-01 16:38:43 -0500

    Unificacion App
    Se unio los elementos del app de la programadora Laura y Luis

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
-import React from 'react';
-import Home from './pages/Home/Home.jsx';
+import React from "react";
+import Home from "./pages/Home/Home.jsx";
+import Index from "./components/Index/Index.jsx";
 
 export default function App() {
-  return <Home />;
+  // Aquí se guarda el índice que se seleccionó
+  const [lastSelectedIndex, setLastSelectedIndex] = useState(0);
+
+  // Esta función se pasa a Index y se ejecuta cuando se hace clic en un botón
+  const handleSelect = (index) => {
+    setLastSelectedIndex(index);
+    console.log(`El último índice seleccionado es: ${index}`);
+  };
+
+  const texts = [
+    "Texto extendido para el botón 1",
+    "Texto extendido para el botón 2",
+    "Texto extendido para el botón 3",
+    "Texto extendido para el botón 4",
+    "Texto extendido para el botón 5",
+    "Texto extendido para el botón 6",
+    "Texto extendido para el botón 7",
+  ];
+
+  return (
+    <>
+      <Home />;
+      <Index
+        onSelect={handleSelect}
+        selectedIndex={lastSelectedIndex}
+        hoverTexts={texts}
+      />
+      {/* ---------------------------------------------------- */}
+      {/* Renderiza el otro componente y le pasas el índice como prop */}
+      {/* ---------------------------------------------------- */}
+      {/*<OtroComponente selectedIndex={lastSelectedIndex} />*/}
+    </>
+  );
 }
