45e044c190b6f62df8e644106ba8a24d29eb0d5b
luisda13 <luisda.martinez.111@gmail.com>
2025-08-04 10:24:22 -0500

    Creacion Card y Fix
    se creo el componente card y se arreglo problemas en el estilo de index

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import Home from "./pages/Home/Home.jsx";
 import Index from "./components/Index/Index.jsx";
 import React, { useState } from "react";
+import Card from "./components/Card/Card.jsx";
+import "./App.css";
 
 export default function App() {
   // Aquí se guarda el índice que se seleccionó
   const [lastSelectedIndex, setLastSelectedIndex] = useState(0);
+  // Aquí se guarda el estado del componente Card
+  const [showCard, setShowCard] = useState(true);
 
-  // Esta función se pasa a Index y se ejecuta cuando se hace clic en un botón
   const handleSelect = (index) => {
-    setLastSelectedIndex(index);
-    console.log(`El último índice seleccionado es: ${index}`);
+    // 1. Oculta la card
+    setShowCard(false);
+
+    // 2. Espera un momento para que la animación de salida termine
+    setTimeout(() => {
+      setLastSelectedIndex(index);
+      // 3. Muestra la card con la nueva información
+      setShowCard(true);
+    }, 300); // 300ms es un buen tiempo para una transición suave
   };
 
   const texts = [
@@ -25,16 +35,20 @@ export default function App() {
   return (
     <>
       <Home />
-      <div>
+      <div className="contenedor-principal">
         <Index
           onSelect={handleSelect}
           selectedIndex={lastSelectedIndex}
           hoverTexts={texts}
         />
-        {/* ---------------------------------------------------- */}
-        {/* Renderiza el otro componente y le pasas el índice como prop */}
-        {/* ---------------------------------------------------- */}
-        {/*<OtroComponente selectedIndex={lastSelectedIndex} />*/}
+        {lastSelectedIndex !== null && (
+          <Card
+            isVisible={showCard}
+            title={texts[lastSelectedIndex]}
+            description={"un pequeño parrafo para saber que este bien"}
+            image="/static/images/cards/contemplative-reptile.jpg"
+          />
+        )}
       </div>
       <div></div>
       <div></div>
